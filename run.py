c4ecfda62eab2ffd8966a8769316634b17dbea82
motoemoto47ark123 <121888254+motoemoto47ark123@users.noreply.github.com>
2023-04-18 21:09:43 -0700

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ path = "replace this with the dir of the files"
 for filename in os.listdir(path):
     original_name, ext = os.path.splitext(filename)
     if "Copy of " in original_name:
-        new_name = original_name.replace("Copy of ", "")
+        new_name = original_name.replace("replace this with what you want to remove from the name of the file ", "")
         os.rename(os.path.join(path, filename), os.path.join(path, new_name + ext))
